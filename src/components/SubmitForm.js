0cc934f2dab2f75d8f95f023fbabdc019aca4f40
daltonlcarr <daltcarr@gmail.com>
2020-03-28 12:58:35 -0700

    submit form set up for createBusiness mutation

--- src/components/SubmitForm.js
+++ src/components/SubmitForm.js
@@ -1,8 +1,56 @@
 import React from 'react';
 import { Button, InputGroup, FormControl } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { API, graphqlOperation } from 'aws-amplify';
+import { createBuisness } from '../graphql/mutations';
 
 export default class SubmitForm extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			yourEmail: '',
+			nameOfBusiness: '',
+			yourName: '',
+			deal: '',
+			siteURL: '',
+			image: '',
+			businesses: []
+		};
+	}
+
+	handleOnChange = (event) => {
+		this.setState({
+			[event.target.name]: event.target.value
+		});
+	};
+
+	addInfo = async (event) => {
+		const { yourEmail, nameOfBusiness, yourName, deal, siteURL, image, businesses } = this.state;
+
+		event.preventDefault();
+
+		const input = {
+			yourEmail,
+			nameOfBusiness,
+			yourName,
+			deal,
+			siteURL,
+			image
+		};
+
+		const result = await API.graphql(graphqlOperation(createBuisness, { input }));
+		const newAddInfo = result.data.createBuisness;
+		const updatedAddInfo = [ newAddInfo, ...businesses ];
+		this.setState({
+			businesses: updatedAddInfo,
+			yourEmail: '',
+			nameOfBusiness: '',
+			yourName: '',
+			deal: '',
+			siteURL: '',
+			image: ''
+		});
+	};
 	render() {
 		return (
 			<div className="row submit-main">
@@ -10,36 +58,75 @@ export default class SubmitForm extends React.Component {
 				<div className="col-xl-6">
 					<InputGroup size="sm" className="mb-3">
 						<InputGroup.Prepend>
-							<InputGroup.Text id="inputGroup-sizing-sm">Email Address</InputGroup.Text>
-						</InputGroup.Prepend>
-						<FormControl aria-label="Small" aria-describedby="inputGroup-sizing-sm" />
-					</InputGroup>
-					<InputGroup size="sm" className="mb-3">
-						<InputGroup.Prepend>
 							<InputGroup.Text id="inputGroup-sizing-sm">Your Name</InputGroup.Text>
 						</InputGroup.Prepend>
-						<FormControl aria-label="Small" aria-describedby="inputGroup-sizing-sm" />
+						<FormControl
+							name="yourName"
+							onChange={this.handleOnChange}
+							aria-label="Small"
+							aria-describedby="inputGroup-sizing-sm"
+						/>
 					</InputGroup>
 					<InputGroup size="sm" className="mb-3">
 						<InputGroup.Prepend>
-							<InputGroup.Text id="inputGroup-sizing-sm">Buisness Name</InputGroup.Text>
+							<InputGroup.Text id="inputGroup-sizing-sm">Email Address</InputGroup.Text>
 						</InputGroup.Prepend>
-						<FormControl aria-label="Small" aria-describedby="inputGroup-sizing-sm" />
+						<FormControl
+							type="text"
+							name="yourEmail"
+							onChange={this.handleOnChange}
+							aria-label="Small"
+							aria-describedby="inputGroup-sizing-sm"
+						/>
 					</InputGroup>
 					<InputGroup size="sm" className="mb-3">
 						<InputGroup.Prepend>
-							<InputGroup.Text id="inputGroup-sizing-sm">Deal you're offering</InputGroup.Text>
+							<InputGroup.Text id="inputGroup-sizing-sm">Name of Buisness</InputGroup.Text>
 						</InputGroup.Prepend>
-						<FormControl aria-label="Small" aria-describedby="inputGroup-sizing-sm" />
+						<FormControl
+							type="text"
+							name="nameOfBusiness"
+							onChange={this.handleOnChange}
+							aria-label="Small"
+							aria-describedby="inputGroup-sizing-sm"
+						/>
 					</InputGroup>
 					<InputGroup size="sm" className="mb-3">
 						<InputGroup.Prepend>
-							<InputGroup.Text id="inputGroup-sizing-sm">
-								Link to landing page of deal offered
-							</InputGroup.Text>
+							<InputGroup.Text id="inputGroup-sizing-sm">Deal</InputGroup.Text>
 						</InputGroup.Prepend>
-						<FormControl aria-label="Small" aria-describedby="inputGroup-sizing-sm" />
+						<FormControl
+							name="deal"
+							onChange={this.handleOnChange}
+							aria-label="Small"
+							aria-describedby="inputGroup-sizing-sm"
+						/>
 					</InputGroup>
+					<InputGroup size="sm" className="mb-3">
+						<InputGroup.Prepend>
+							<InputGroup.Text id="inputGroup-sizing-sm">Site URL</InputGroup.Text>
+						</InputGroup.Prepend>
+						<FormControl
+							name="siteURL"
+							onChange={this.handleOnChange}
+							aria-label="Small"
+							aria-describedby="inputGroup-sizing-sm"
+						/>
+					</InputGroup>
+					<InputGroup size="sm" className="mb-3">
+						<InputGroup.Prepend>
+							<InputGroup.Text id="inputGroup-sizing-sm">Upload Image</InputGroup.Text>
+						</InputGroup.Prepend>
+						<FormControl
+							name="image"
+							onChange={this.handleOnChange}
+							aria-label="Small"
+							aria-describedby="inputGroup-sizing-sm"
+						/>
+					</InputGroup>
+					<Button onClick={() => this.addInfo()} className="home-main__button" variant="primary" size="lg">
+						Submit
+					</Button>{' '}
 				</div>
 			</div>
 		);
