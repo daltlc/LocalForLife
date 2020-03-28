5e67255d13ef845dc9608c51f6b834655891fe2c
daltonlcarr <daltcarr@gmail.com>
2020-03-28 02:02:19 -0700

    basic front end done, now to work on bringing mutations and queries in to begin storing data to DynamoDB

--- src/components/HomeTop3.js
+++ src/components/HomeTop3.js
@@ -5,10 +5,15 @@ export default class HomeTop3 extends React.Component {
 	render() {
 		return (
 			<div className="home-top-3 row">
-				<div className="item col-xl-4" />
-				<div className="item col-xl-4" />
-
-				<div className="item col-xl-4" />
+				<div className="item col-xl-4">
+					<h1>hello</h1>
+				</div>
+				<div className="item col-xl-4">
+					<h1>hello</h1>
+				</div>
+				<div className="item col-xl-4">
+					<h1>hello</h1>
+				</div>{' '}
 			</div>
 		);
 	}
